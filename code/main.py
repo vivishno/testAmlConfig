c6bb0c2915e05df9e34b709b5fc5330e0dd01a50
vivishno <64971172+vivishno@users.noreply.github.com>
2020-06-10 08:52:20 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -63,13 +63,13 @@ def main():
     service_principal_id=azure_credentials.get("clientId", "")
     service_principal_password=azure_credentials.get("clientSecret", "")
     
-    command = ('az login --service-principal --username {APP_ID} --password {PASSWORD} --tenant {TENANT_ID}').format(
-            APP_ID=service_principal_id, PASSWORD=service_principal_password, TENANT_ID=tenant_id)
-    try:
-        app_create = subprocess.check_output(command, shell=True)
-        print(app_create)
-    except Exception as ex:
-        print(ex)
+   # command = ('az login --service-principal --username {APP_ID} --password {PASSWORD} --tenant {TENANT_ID}').format(
+    #        APP_ID=service_principal_id, PASSWORD=service_principal_password, TENANT_ID=tenant_id)
+    #try:
+     #   app_create = subprocess.check_output(command, shell=True)
+      #  print(app_create)
+    #except Exception as ex:
+     #   print(ex)
     print(deploy_functionApp(template_file_file_path ,template_params_file_path , resource_group))
     
 
