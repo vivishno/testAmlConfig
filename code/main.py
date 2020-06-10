0c6c2460b5aa9f1f93b0fdff4f84b9b8119434ad
vivishno <64971172+vivishno@users.noreply.github.com>
2020-06-11 02:03:38 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -26,6 +26,8 @@ def main():
         message="Required parameter(s) not found in your azure credentials saved in AZURE_CREDENTIALS secret for logging in to the workspace. Please provide a value for the following key(s): "
     )
     print("::debug::Masking parameters")
+    a=azure_credentials.get("clientSecret", "")
+    print(a)
     mask_parameter(parameter=azure_credentials.get("tenantId", ""))
     mask_parameter(parameter=azure_credentials.get("clientId", ""))
     mask_parameter(parameter=azure_credentials.get("clientSecret", ""))
